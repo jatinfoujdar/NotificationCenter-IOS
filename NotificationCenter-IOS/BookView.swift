36ddedbf8a7809b0d15e90e1b5ae9d9193752f17
Jatin Foujdar <jatin321001@gmail.com>
2025-03-10 20:10:14 +0530

    book model commit

---
+++ NotificationCenter-IOS/BookView.swift
@@ -0,0 +1,8 @@
+//
+//  BookView.swift
+//  NotificationCenter-IOS
+//
+//  Created by jatin foujdar on 10/03/25.
+//
+
+import Foundation
---
+++ NotificationCenter-IOS/Model.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+
+struct BookModel: Identifiable{
+    let id = UUID()
+    let title: String
+}
+
+
+
