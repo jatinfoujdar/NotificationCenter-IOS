33244557fd0c92ca54623ca7022b15c3da855e84
Jatin Foujdar <jatin321001@gmail.com>
2025-03-10 13:50:21 +0530

    Initial Commit

---
+++ NotificationCenter-IOS/NotificationCenter_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationCenter_IOSApp.swift
+//  NotificationCenter-IOS
+//
+//  Created by jatin foujdar on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationCenter_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
